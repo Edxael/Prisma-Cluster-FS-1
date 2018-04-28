035be9c2fb54463835fff7ffa5d63378fc6bb1f6
Edxael <zerphyr.r@gmail.com>
2018-04-28 17:12:51 -0600

    Fix page not found Errors when refresh

--- 02-front-end/src/MyApp/00-Main.jsx
+++ 02-front-end/src/MyApp/00-Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ApolloProvider } from "react-apollo"
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { HashRouter, Route, Link } from 'react-router-dom'
 import './Style/style.css'
 import { client } from './PrismaEndPoint/EndPoint'
